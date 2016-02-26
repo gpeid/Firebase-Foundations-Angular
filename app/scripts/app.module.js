5d6cf0e598bcf5f5f23591b59a4268f249ec4b6c
George <ghdiep@gmail.com>
2016-02-26 09:07:29 -0500

    first commit

---
+++ app/scripts/app.module.js
@@ -0,0 +1,14 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('app', [
+			'ngRoute',
+			'firebase'
+		]);
+		//run arbitrary angular code with run() block
+		// .run(function(){
+		// 	console.log('in app module')
+		// });
+
+})();
